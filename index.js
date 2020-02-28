e970e82828738367fd8dd6e6d82bcd1dc4739af5
万古云霄 <zhangpanqin@outlook.com>
2020-02-28 19:39:17 +0800

    feat:添加 依赖 fly-npm 的依赖 fly-use-npm

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const obj = {
-    version: 2
+import flyNpm from 'fly-npm';
+const obj = () => {
+    console.log('引用的 fly-npm 版本为:', flyNpm.version);
+    if (flyNpm.version > 1) {
+        throw new Error('版本大于 1');
+    }
 }
 export default obj;
